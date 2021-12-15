f2a98330320983c88f5ba592fb1c84a41df6dee9
Paulo Ricardo <piokinovaes@gmail.com>
2021-12-15 10:42:15 -0300

    Add project files.

---
+++ PLSKTodoApp/Forms/Main.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PLSKTodoApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Location = new System.Drawing.Point(145, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(498, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PLSK Todo App";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 251);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
---
+++ PLSKTodoApp/Forms/Main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PLSKTodoApp
+{
+    public partial class Main : Form
+    {
+        CheckBox cbox;
+        Button btnDelete;
+        private int Counter;
+        public Main()
+        {
+            InitializeComponent();
+            Counter = 0;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                AddItem(textBox1.Text);
+        }
+        private void AddItem(string item)
+        {
+            // ComboBox
+            cbox = new CheckBox();
+            cbox.Name = "cbox" + Counter;
+            if (item.Length > 17)
+                item = item.Substring(0, 17) + "...";
+            cbox.Text = item;
+            cbox.Location = new Point(textBox1.Location.X, textBox1.Location.Y);
+            cbox.Size = new Size(textBox1.Size.Width, textBox1.Size.Height);
+            cbox.Anchor = AnchorStyles.Top;
+            cbox.CheckedChanged += Cbox_CheckedChanged;
+            Controls.Add(cbox);
+
+            // Delete Button
+            btnDelete = new Button();
+            btnDelete.Name = "btnDelete" + Counter;
+            btnDelete.Text = "X";
+            btnDelete.Size = new Size(25, 25);
+            btnDelete.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDelete.ForeColor = Color.Red;
+            btnDelete.Location = new Point(textBox1.Location.X + textBox1.Width, textBox1.Location.Y);
+            btnDelete.Anchor = AnchorStyles.Top;
+            btnDelete.Click += BtnDelete_Click;
+            Controls.Add(btnDelete);
+
+            // add to counter
+            Counter++;
+            // correct textbox to the next location
+            textBox1.Location = new Point(textBox1.Location.X, textBox1.Location.Y+30);
+            textBox1.Text = "";
+        }
+
+        private void Cbox_CheckedChanged(object? sender, EventArgs e)
+        {
+            cbox = sender as CheckBox;
+            if (cbox.Checked)
+                cbox.Font = new Font("Segoe UI", 9F, FontStyle.Strikeout, GraphicsUnit.Point);
+            else
+                cbox.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+        }
+
+        private void BtnDelete_Click(object? sender, EventArgs e)
+        {
+            btnDelete = sender as Button;
+            int bId = Convert.ToInt32(btnDelete.Name.Substring(9));
+            int cId;
+            foreach (CheckBox c in Controls.OfType<CheckBox>())
+            {
+                cId = Convert.ToInt32(c.Name.Substring(4));
+                if (cId == bId)
+                    Controls.Remove(c);
+                if (cId > bId)
+                    c.Location = new Point(c.Location.X, c.Location.Y-30);
+                Control d = Controls["btnDelete" + cId];
+                d.Location = new Point(d.Location.X, c.Location.Y);
+                
+            }
+                
+            Controls.Remove(btnDelete);
+            textBox1.Location = new Point(textBox1.Location.X, textBox1.Location.Y - 30);
+        }
+    }
+}
